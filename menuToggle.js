9fea6a010df2b50305cf060645d130e4b067eefc
Diego Procopio <diegprocopio98@gmail.com>
2022-04-30 20:43:53 -0300

    explementando menu-toggle

---
+++ menuToggle.js
@@ -0,0 +1,10 @@
+let show = true;
+
+const menuToggle = document.querySelector(".menu-toggle");
+
+menuToggle.addEventListener("click", () => {
+  document.body.style.overflow = show ? "hidden" : "initial";
+
+  menuSection.classList.toggle("on", show);
+  show = !show;
+});
